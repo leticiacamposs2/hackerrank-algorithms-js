c9302feb33b132a96e618f21bb4ffe29e4874ae9
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2020-06-20 21:10:13 -0300

    compare triplets

---
+++ 02-compare-the-triplets.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function (inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function () {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the compareTriplets function below.
+function compareTriplets(a, b) {
+
+    let alice = 0;
+    let bob = 0;
+    const result = [];
+
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] > b[i]) {
+            alice += 1;
+        } else if (b[i] > a[i]) {
+            bob += 1;
+        }
+    }
+
+    result.push(alice, bob);
+    return result;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const b = readLine().replace(/\s+$/g, '').split(' ').map(bTemp => parseInt(bTemp, 10));
+
+    const result = compareTriplets(a, b);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
